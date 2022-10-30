d3ac353f46cc9ca29428e0d4478c6c6155a877d6
Guilecompo <117016354+Guilecompo@users.noreply.github.com>
2022-10-30 22:10:40 +0800

    Add files via upload

---
+++ SimpleBankApplication.java
@@ -0,0 +1,15 @@
+package simplebankapplication;
+
+
+public class SimpleBankApplication 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        SBA_MainDetails.CHOOSE();
+        SBA_MainProject.Choices();
+        
+    }
+    
+}
